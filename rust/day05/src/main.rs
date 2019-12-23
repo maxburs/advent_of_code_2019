40fcb996eecaaa4c50e84d939b227f418f0a3043
Maxwell Burson <maxwellburson@gmail.com>
2019-12-23 13:53:10 -0800

    Added day 5 part 1 given tests

--- rust/day05/src/main.rs
+++ rust/day05/src/main.rs
@@ -118,8 +118,14 @@ fn main() -> Result<(), std::io::Error> {
 
 #[test]
 fn tests() {
+
+    // Day 2 tests
     assert_eq!(run(vec![1,0,0,0,99]).unwrap(), vec![2,0,0,0,99]);
     assert_eq!(run(vec![2,3,0,3,99]).unwrap(), vec![2,3,0,6,99]);
     assert_eq!(run(vec![2,4,4,5,99,0]).unwrap(), vec![2,4,4,5,99,9801]);
     assert_eq!(run(vec![1,1,1,4,99,5,6,0,99]).unwrap(), vec![30,1,1,4,2,5,6,0,99]);
+
+    // Day 5 tests
+    assert_eq!(run(vec![1002,4,3,4,33]).unwrap(), vec![1002,4,3,4,99]);
+    assert_eq!(run(vec![1101,100,-1,4,0]).unwrap(), vec![1101,100,-1,4,99]);
 }
