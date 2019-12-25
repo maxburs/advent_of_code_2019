f4fc85428f36fc94e94d143470c511525f5074c2
Maxwell Burson <maxwellburson@gmail.com>
2019-12-24 16:31:00 -0800

    Fixed day 5 input and output handling

--- rust/day05/src/main.rs
+++ rust/day05/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 type Memory = Vec<isize>;
 
-#[derive(Debug,Eq,PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 enum ParameterMode {
     Position,
     Immediate,
@@ -14,16 +14,18 @@ struct InstructionArguments<'a> {
 }
 
 impl InstructionArguments<'_> {
-    fn param(&self, position: usize)-> isize {
+    fn param(&self, position: usize) -> isize {
         let mode = find_mode(self.memory[self.instruction_pointer], position);
         match mode {
-            ParameterMode::Position => self.memory[self.memory[self.instruction_pointer + position] as usize],
+            ParameterMode::Position => {
+                self.memory[self.memory[self.instruction_pointer + position] as usize]
+            }
             ParameterMode::Immediate => self.memory[self.instruction_pointer + position],
         }
     }
 }
 
-// TODO: make this fast
+// TODO: Don't use strings for this
 fn find_mode(opcode: isize, position: usize) -> ParameterMode {
     let mut opcode: Vec<char> = opcode.to_string().chars().into_iter().collect();
     opcode.reverse();
@@ -37,31 +39,31 @@ fn find_mode(opcode: isize, position: usize) -> ParameterMode {
 #[test]
 fn find_mode_tests() {
     assert_eq!(1002 % 100, 2);
-    assert_eq!(find_mode(1002, 1), ParameterMode::Position); 
-    assert_eq!(find_mode(1002, 2), ParameterMode::Immediate); 
-    assert_eq!(find_mode(1002, 3), ParameterMode::Position); 
+    assert_eq!(find_mode(1002, 1), ParameterMode::Position);
+    assert_eq!(find_mode(1002, 2), ParameterMode::Immediate);
+    assert_eq!(find_mode(1002, 3), ParameterMode::Position);
 }
 
-fn add(args: InstructionArguments<'_>)  {
+fn add(args: InstructionArguments<'_>, _input: &mut Vec<isize>, _output: &mut Vec<isize>) {
     let val = args.param(1) + args.param(2);
     let pos = args.memory[args.instruction_pointer + 3];
     args.memory[pos as usize] = val;
 }
 
-fn multiply(args: InstructionArguments<'_>)  {
+fn multiply(args: InstructionArguments<'_>, _input: &mut Vec<isize>, _output: &mut Vec<isize>) {
     let val = args.param(1) * args.param(2);
     let pos = args.memory[args.instruction_pointer + 3];
     args.memory[pos as usize] = val;
 }
 
-fn save(args: InstructionArguments<'_>)  {
-    let val = args.memory[args.instruction_pointer];
+fn read_input(args: InstructionArguments<'_>, input: &mut Vec<isize>, _output: &mut Vec<isize>) {
+    let val = input.pop().unwrap();
     let pos = args.memory[args.instruction_pointer + 1];
     args.memory[pos as usize] = val;
 }
 
-fn output(args: InstructionArguments<'_>)  {
-    println!("# {}", args.memory[args.instruction_pointer + 1]);
+fn print_output(args: InstructionArguments<'_>, _input: &mut Vec<isize>, output: &mut Vec<isize>) {
+    output.push(args.memory[args.instruction_pointer + 1]);
 }
 
 fn load() -> Result<Memory, std::io::Error> {
@@ -69,63 +71,71 @@ fn load() -> Result<Memory, std::io::Error> {
         .split(',')
         .map(|s| s.trim())
         .filter(|s| *s != "")
-        .map(|s| s.parse::<isize>().unwrap()).collect())
+        .map(|s| s.parse::<isize>().unwrap())
+        .collect())
 }
 
-fn run(mut memory: Memory) -> Result<Memory, String>{
+fn run(memory: &mut Memory, mut input: Vec<isize>) -> Vec<isize>{
     let mut instruction_pointer = 0;
+
+    let mut output = Vec::new();
 
     loop {
         let opcode = memory[instruction_pointer] % 100;
-        let (instruction, values_in_instruction): (Box<dyn Fn(InstructionArguments<'_>)>, usize) = match opcode {
-            1 => (Box::new(&add), 4),
-            2 => (Box::new(&multiply), 4),
-            3 => (Box::new(&save), 3),
-            4 => (Box::new(&output), 3),
-            99 => return Ok(memory),
-            _ => return Err(format!("Invalid opcode {} at instruction_pointer {}", opcode, instruction_pointer)),
-        };
-        instruction(InstructionArguments { memory: &mut memory, instruction_pointer });
+        let (instruction, values_in_instruction): (Box<dyn Fn(InstructionArguments<'_>, &mut Vec<isize>, &mut Vec<isize>)>, usize) =
+            match opcode {
+                1 => (Box::new(&add), 4),
+                2 => (Box::new(&multiply), 4),
+                3 => (Box::new(&read_input), 2),
+                4 => (Box::new(&print_output), 2),
+                99 => return output,
+                _ => panic!(
+                    "Invalid opcode {} at instruction_pointer {}",
+                    opcode, instruction_pointer
+                ),
+            };
+        instruction(InstructionArguments {
+            memory,
+            instruction_pointer,
+        }, &mut input, &mut output);
         instruction_pointer += values_in_instruction;
     }
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let memory = load()?;
-
-    for noun in 0..100 {
-        for verb in 0..100 {
-
-            let mut prospect = memory.clone();
-            prospect[1] = noun;
-            prospect[2] = verb;
-
-            match run(prospect) {
-                Ok(result) => {
-                    if result[0] == 19690720 {
-                        println!("noun: {}, verb: {}", noun, verb);
-                        return Ok(());
-                    }
-                },
-                Err(_) => { println!("hello world")}
-            }
-        }
-    }
-    
-    println!("No answer found");
+    let mut memory = load()?;
+    let output = run(&mut memory, vec![1]);
+    println!("Ouputput: {:#?}", output);
     Ok(())
 }
 
 #[test]
 fn tests() {
+    // # Day 2 tests
+    assert_eq!(run(vec![1, 0, 0, 0, 99]).unwrap(), vec![2, 0, 0, 0, 99]);
+    assert_eq!(run(vec![2, 3, 0, 3, 99]).unwrap(), vec![2, 3, 0, 6, 99]);
+    assert_eq!(
+        run(vec![2, 4, 4, 5, 99, 0]).unwrap(),
+        vec![2, 4, 4, 5, 99, 9801]
+    );
+    assert_eq!(
+        run(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]).unwrap(),
+        vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
+    );
 
-    // Day 2 tests
-    assert_eq!(run(vec![1,0,0,0,99]).unwrap(), vec![2,0,0,0,99]);
-    assert_eq!(run(vec![2,3,0,3,99]).unwrap(), vec![2,3,0,6,99]);
-    assert_eq!(run(vec![2,4,4,5,99,0]).unwrap(), vec![2,4,4,5,99,9801]);
-    assert_eq!(run(vec![1,1,1,4,99,5,6,0,99]).unwrap(), vec![30,1,1,4,2,5,6,0,99]);
-
-    // Day 5 tests
-    assert_eq!(run(vec![1002,4,3,4,33]).unwrap(), vec![1002,4,3,4,99]);
-    assert_eq!(run(vec![1101,100,-1,4,0]).unwrap(), vec![1101,100,-1,4,99]);
+    // # Day 5 tests
+    // From instructions
+    assert_eq!(
+        run(vec![1002, 4, 3, 4, 33]).unwrap(),
+        vec![1002, 4, 3, 4, 99]
+    );
+    assert_eq!(
+        run(vec![1101, 100, -1, 4, 0]).unwrap(),
+        vec![1101, 100, -1, 4, 99]
+    );
+    // Homemade
+    assert_eq!(
+        run(vec![1101, 100, -1, 4, 0, 1002, 4, 3, 8, 33]).unwrap(),
+        vec![1101, 100, -1, 4, 0, 1002, 4, 3, 4, 99]
+    );
 }
